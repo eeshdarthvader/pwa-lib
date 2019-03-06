ec01150f06089013cd3c408b7b3964a01a289daf
eeshtyagi <eesh.tyagi@gmail.com>
2019-03-06 12:06:38 +0530

    Adding more components in lib

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,5 +2,33 @@ import "../styles/app.scss";
 
 import Button from "./Buttons";
 import { AvailabiltyBadge, CodeBadge, StatusBadge, LowSeat } from "./Badges";
+import { Shake } from "./Animate";
+import { Slide } from "./Animate";
+import { Ripple } from "./Animate";
+import AutoComplete from "./Autocomplete";
+import CashbackModal from "./CashbackModal";
+import Deal from "./Deal";
+import Divider from "./Divider";
+import Fab from "./Fab";
+import Footer from "./Footer";
+import { List, ListItem, VirtualList } from "./List";
 
-export { Button, AvailabiltyBadge, CodeBadge, StatusBadge, LowSeat };
+export {
+  Button,
+  AvailabiltyBadge,
+  CodeBadge,
+  StatusBadge,
+  LowSeat,
+  Shake,
+  Slide,
+  Ripple,
+  AutoComplete,
+  CashbackModal,
+  Deal,
+  Divider,
+  Fab,
+  Footer,
+  List,
+  ListItem,
+  VirtualList
+};
